bc56ffdf0b0103407d1c0d5a512dbbb4e52b3653
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-04-18 21:28:02 -0400

    added heroicons

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,23 @@
+import Image from 'next/image'
 import React from 'react'
+import { BellIcon, ChatIcon, ChevronDownIcon, HomeIcon, UserGroupIcon, ViewGridIcon } from '@heroicons/react/solid';
+import { FlagIcon, PlayIcon, SearchIcon, ShoppingCartIcon } from '@heroicons/react/outline';
+
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div>
+            <Image className="rounded-lg"
+                src="https://cdn3.iconfinder.com/data/icons/capsocial-round/500/facebook-512.png"
+                width={40}
+                height={40}
+                layout="fixed"
+                alt="fbIcon"
+            />
+            <div>
+                <input type="text" placeholder="Search..." />
+            </div>
+        </div>
     )
 }
 export default Header
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['localhost', 'cdn3.iconfinder.com',
+    'platform-lookaside.fbsbx.com', 'firebasestorage.googleapis.com']
+  }
 }
-
 module.exports = nextConfig
