2541fb38f41bac9e5b4f3289082ee7accee9881b
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-04-19 23:00:45 -0400

    added next-auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,12 @@
+import NextAuth from "next-auth/next"
+import Providers from "next-auth/providers"
+
+
+export default NextAuth({
+    Providers: [
+        Providers.Facebook({
+            clientId: process.env.FACEBOOK_CLIENT_ID,
+            clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+        })
+    ]
+})
