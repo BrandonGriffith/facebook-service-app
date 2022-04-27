3838c9fb07ae921c8724f009ea2e915511c3eefb
BrandonGriffith <BrandonGriffith92@gmail.com>
2022-04-26 22:53:21 -0400

    added post profile icon

--- components/main_feed/Post.tsx
+++ components/main_feed/Post.tsx
@@ -1,6 +1,20 @@
+import Image from "next/image"
+
 const Post = (props: any) => {
     return (
-        <div>Post</div>
+        <div className="flex flex-col">
+            <div>
+                <div>
+                    <Image
+                        src={props.image}
+                        alt={props.image}
+                        height={40}
+                        width={40}
+                        className="rounded-full"
+                    />
+                </div>
+            </div>
+        </div>
     )
 }
 export default Post
